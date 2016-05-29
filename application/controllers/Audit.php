67b3f9dec6991e0074cac9a873aa49c6fb9e8b6c
spokzers <spokzers@gmail.com>
2016-05-29 05:45:32 +0500

    major audit report changes

--- application/controllers/Audit.php
+++ application/controllers/Audit.php
@@ -44,7 +44,7 @@ class Audit extends CI_Controller {
 		$this->load->view('base');
 		$this->load->view('audit_view', $data);
 		$this->load->view('footer');
-		$this->load->view('audit_view_js', $data);
+		$this->load->view('audit_view_js');
 	}
 
 	public function create(){
